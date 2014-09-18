0bfb427dd2f32a1c9e2d492b2dc31db540daa356
Jesse Showalter <iamjesseshowalter@gmail.com>
2014-09-18 13:00:45 -1000

    started over

--- js/app.js
+++ js/app.js
@@ -1,31 +1,21 @@
 $(document).ready(function(){
 
-	//Allow user to select number to play by
-	var numPrompt = function(numChoose){
-		numChoose = prompt("Choose a number to play \"FizzBuzz\"");
-		// console.log(numChoose);
-		return numChoose;
-		alert(numChoose);
-	};
-	numPrompt();
-
-
+	// Allow user to select number to play by
+	var userChoice = prompt("Choose a number to play \"FizzBuzz\"");
 	// Alert them of the game starting
-	// alert("You have chosen to play FizzBuzz up to " + userChoice + " ... Ready to get started?");	
-
-
+	alert("You Chose to play FizzBuzz up to " + userChoice + " , Ready to get started?");
 	
-	// // Conditional Logic for fizz an buzz and fizzbuzz
-	// for(var i = 1; i <= userChoice; i++){
-	//     if(i%15 === 0){
-	//     	$('ul').append("<li>Fizz Buzz!</li>");
-	//     }else if (i%3 === 0) {
-	//     	$('ul').append("<li>Fizz</li>");
-	//     }else if (i%5 === 0) {
-	//     	$('ul').append("<li>Buzz</li>");
-	//     }else {
-	//     	$('ul').append("<li>" + i + "</li>");
-	//     };
-	// }
+	// Conditional Logic for fizz an buzz and fizzbuzz
+	for(var i = 1; i <= userChoice; i++){
+	    if(i%15 === 0){
+	    	$('ul').append("<li>Fizz Buzz!</li>");
+	    }else if (i%3 === 0) {
+	    	$('ul').append("<li>Fizz</li>");
+	    }else if (i%5 === 0) {
+	    	$('ul').append("<li>Buzz</li>");
+	    }else {
+	    	$('ul').append("<li>" + i + "</li>");
+	    };
+	}
 
 });
