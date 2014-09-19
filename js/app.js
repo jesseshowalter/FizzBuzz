69cb6a7a6da9d7e6e4a5623702648503f416c9e5
Jesse Showalter <iamjesseshowalter@gmail.com>
2014-09-18 16:02:48 -1000

    re working

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
+// user prompted to enter number
+// create 1 named function that takes the inputed integer as argument and runs the game
+// convert inputed value from string to number
+// create conditonal if number is NaN, alert for a number
+
+
+
+
 // Allow user to select number to play by
 var userChoice = prompt("Choose a number to play \"FizzBuzz\"");
-var num = parseInt(userChoice, 10);
-var valueIsNaN = (num !== num)
+// var num = parseInt(userChoice, 10);
+var num = (+userChoice)
+// var valueIsNaN = (num !== num)
 
 
 var runGame = function(num){
+	if(valueIsNaN === true){
+		alert("Sorry, This is not a number, please try again and choose an actual integer number!");
+		prompt("Choose a number to play \"FizzBuzz\"");
+	}
 
-	// while(valueIsNaN === false){
-	// 	// alert("Sorry, This is not a number, please try again and choose an actual integer number!");
-	// 	// prompt("Choose a number to play \"FizzBuzz\"");
-	// 	console.log("test");
-	// }
-
-	for(var i = 1; i <= userChoice; i++){
+	for(var i = 1; i <= num; i++){
 	    if(i%15 === 0){
 	    	$('ul').append("<li>Fizz Buzz!</li>");
 	    }else if (i%3 === 0) {
@@ -25,15 +32,7 @@ var runGame = function(num){
 	}
 };
 
-
-
 runGame();	
 
 
 
-
-
-// user prompted to enter number
-// create 1 named function that takes the inputed integer as argument and runs the game
-// convert inputed value from string to number
-// create conditonal if number is NaN, alert for a number
